96abe54bdf0b652145f09e438b9c5d84fef24e72
GabrielDevXD1 <84517125+GabrielDevXD@users.noreply.github.com>
2022-10-03 19:11:31 -0300

    fix(project): error csv archive

--- src/product/archivecsv.ts
+++ src/product/archivecsv.ts
@@ -16,21 +16,17 @@ const fields = [
 const opts = { fields };
 
 class exportFiles {
-    tocsv = function (news) {
-        try{
-            const csv = await json2csv.parseAsync(news, opts);
-            const filenames = uuid.v4() + ".csv"
-            fs.writeFile("./exports/"+ filenames, csv, function(err){
-                if(err) throw err;
-                console.log("Arquivo salvo com sucesso")
-                
-            });
-            return filenames
-        } catch (error) {
-            console.log(error)
-            
-        }
+  tocsv = async function (news) {
+    try {
+      const csv = await json2csv.parseAsync(news, opts);
+      const filenames = uuid.v4() + '.csv';
+      fs.writeFile('./exports/' + filenames, csv, function (err) {
+        if (err) throw err;
+        console.log('Arquivo salvo com sucesso');
+      });
+      return filenames;
+    } catch (error) {
+      console.log(error);
     }
-
-
-
+  };
+}
